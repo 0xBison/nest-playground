e7508b78add829b9809cbc7722568346ba36c51b
0xBison <196279375+0xBison@users.noreply.github.com>
2025-02-07 16:07:13 +0800

    feature: example of how to use nest-git-info package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GitInfoModule } from 'nest-git-info';
 
 @Module({
-  imports: [],
+  imports: [
+    GitInfoModule.register({
+      // Controller configuration
+      routePath: 'version', // Custom endpoint path (default: 'git-info')
+      swaggerTag: 'Version Info', // Custom Swagger documentation tag (default: 'git-info')
+
+      // Feature flags
+      disableSwagger: false, // Disable Swagger documentation (default: false)
+      disableController: false, // Disable the controller entirely (default: false)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
